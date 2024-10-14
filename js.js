c52ddfbf14c02c2c8f39e3f7b2a97243e8169e23
teecodes444 <171301131+teecodes444@users.noreply.github.com>
2024-10-14 18:00:24 -0500

    Add if/else for operate(); remove parameters from operator()s

--- js.js
+++ js.js
@@ -2,24 +2,30 @@ let num1;
 let num2;
 let operator; 
 
-const add = (x, y) => {
+const add = () => {
     return x + y;
 }
 
-const subtract = (x, y) => {
+const subtract = () => {
     return x - y;
 }
 
-const multiply = (x, y) => {
+const multiply = () => {
     return x * y;
 }
 
-const divide = (x, y) => {
+const divide = () => {
     return x / y;
 }
 
 const operate = (x, y) => {
     if (operator === "+") {
         add();
+    } else if (operator === '-') {
+        subtract();
+    } else if (operator === '*') {
+        multiply();
+    } else if (operator === '/') {
+        divide();
     }
 };
